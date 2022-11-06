ad0c2756c4013b937f0700183405eac29486f1ed
teddy.andria <teddy.andriamam@gmail.com>
2022-11-06 13:27:00 +0100

    modif form.php

--- form.php
+++ form.php
@@ -1,34 +1,3 @@
-<?php
-
-$errors = [];
-
-if ($_SERVER['REQUEST_METHOD'] === "POST") {
-
-    $uploadDir = 'public/uploads/';
-
-    $uploadFile = $uploadDir . basename($_FILES['avatar']['name']);
-
-    $extension = pathinfo($_FILES['avatar']['name'], PATHINFO_EXTENSION);
-
-    $authorizedExtensions = ['jpg', 'jpeg', 'png'];
-
-    $maxFileSize = 1000000;
-
-    if ((!in_array($extension, $authorizedExtensions))) {
-        $errors[] = 'Veuillez sélectionner une image de type Jpg ou Jpeg ou Png !';
-    }
-
-    if (file_exists($_FILES['avatar']['tmp_name']) && filesize($_FILES['avatar']['tmp_name']) > $maxFileSize) {
-        $errors[] = "Votre fichier doit faire moins de 1M !";
-    }
-
-    move_uploaded_file($_FILES['avatar']['tmp_name'], $uploadFile);
-}
-
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
